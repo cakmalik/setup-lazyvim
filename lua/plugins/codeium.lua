c8a1322af6eb3d3ec0b980ac7e0ca8b38f2ef1c4
cakmalik <hasinilmalik@gmail.com>
2025-03-08 02:35:53 +0700

    coodeium solve

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,20 +1,4 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  opts = {
-    filters = {
-      dotfiles = false, -- Menampilkan file yang diawali titik (.env, .log)
-      custom = {}, -- Pastikan tidak ada filter tambahan
-    },
-    git = {
-      enable = true, -- Pastikan ini true jika menggunakan Git
-      ignore = false, -- Agar file yang di-ignore Git tetap muncul
-    },
-    view = {
-      width = 30,
-      side = "left",
-    },
-  },
-  config = function(_, opts)
-    require("nvim-tree").setup(opts)
-  end,
+  "Exafunction/codeium.vim",
+  event = "BufEnter",
 }
